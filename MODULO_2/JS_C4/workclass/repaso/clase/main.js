03572886a0a1bab03ab02fb63b3276b6a10bdb7b
Daniel Canon <danielycm02@gmail.com>
2024-05-22 21:17:13 -0500

    clase_4

--- MODULO_2/JS_C4/workclass/repaso/clase/main.js
+++ MODULO_2/JS_C4/workclass/repaso/clase/main.js
@@ -26,19 +26,19 @@
 // else if (condicion){Lo que quieres hacer, si se cumple la condicion}
 // else {Lo que quieres hacer, si no se cumple la condicion}
 
-const country = prompt("Escriba el pais de nacimiento");
+// const country = prompt("Escriba el pais de nacimiento");
 
-if(country === "colombia"){
-    document.write("Eres colombiano")
-} else if (country === "panama"){
-    document.write("Eres panameño")
-} else if (country === "venezuela"){
-    document.write("Eres venezonalo")
-} else if (country === "peru"){
-    document.write("Eres peruano")
-} else {
-    document.write("Digita otra nacionalidad")
-}
+// if(country === "colombia"){
+//     document.write("Eres colombiano")
+// } else if (country === "panama"){
+//     document.write("Eres panameño")
+// } else if (country === "venezuela"){
+//     document.write("Eres venezonalo")
+// } else if (country === "peru"){
+//     document.write("Eres peruano")
+// } else {
+//     document.write("Digita otra nacionalidad")
+// }
 
 
 // RETO (if/else if/else)
@@ -51,3 +51,35 @@ if(country === "colombia"){
 // Los menores de 18 años (sean masculino o femenino) -> NO PUEDEN ENTRAR
 // Las mujeres siempre entran gratis
 // Los mayores de 18 años -> Pagan un cover de $30.000
+
+// ---------------------------------------------------------------------------
+
+// RETO (2)
+// Crear un programa que pida a él usuario un color de camiseta y una talla (S/M/L/XL)
+//El programa debe decirle a él usario cuantas camisetas hay disponibles:
+
+// azules de talla L -> 4 camisetas
+// azules de talla M -> 10 camisetas
+// azules de talla S -> 6 camisetas
+// amarillas de talla M -> 8 camisetas
+// verde de tallas S -> 2 camisetas
+// de los demás colores y tallas no hay disponibles
+
+const color = prompt("Digita el color de la camiseta").toLowerCase(); // Minuscula
+const talla = prompt("Inserte la talla de tu camiseta").toUpperCase(); // Mayuscula
+
+if(talla === "L" && color === "azul"){
+    document.write("Hay 4 camisetas de color azul")
+} else if(talla === "M" && color === "azul"){
+    document.write("Hay 10 camisetas de color azul")
+} else if(talla === "S" && color === "azul"){
+    document.write("Hay 6 camisetas de color azul")
+} else if(talla === "M" && color === "amarillo"){
+    document.write("Hay 8 camisetas de color amarillo")
+} else if(talla === "S" && color === "verde"){
+    document.write("Hay 2 camisetas de color verde")
+} else {
+    document.write("No hay disponibles")
+}
+
+
