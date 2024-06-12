519d2ce628d987c8570ad5df41bc11deb3e0e0c0
Daniel Canon <danielycm02@gmail.com>
2024-06-11 20:06:46 -0500

    clase_11

---
+++ MODULO_2/JS_C11/workclass/changeList/main.js
@@ -0,0 +1,9 @@
+// document.write("Hello")
+const listaParticipante = document.getElementById("list-1")
+
+const addToList = () => {
+    const name = "Walter";
+    listaParticipante.innerHTML = listaParticipante.innerHTML + "<li>" + name + "</li>";
+}
+
+suma = suma + 1
---
+++ MODULO_2/JS_C11/workclass/changeText/main.js
@@ -0,0 +1,7 @@
+// document.write("Hola mundo")
+
+const parrafo = document.getElementById("text-1");
+
+function changeText () {
+    parrafo.innerHTML = "Este parrafo cambio por medio de JS"
+}
