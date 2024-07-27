c22b3dbf40368d2c9dd2b9fdddc2e4641b9adb99
gustavo morales <brmorales@alianza.com.co>
2024-07-26 23:24:47 -0500

    GustavoMorales: ejercicio practico

---
+++ MODULO_4/Clase-3/ejercicioClase3/src/components/descripcion/descripcionComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Descripcion = (props) => {
+    console.log(props,'descripcion');
+  return (
+    <div>
+        <h2>{props.descripcion}</h2>
+    </div>
+    
+  )
+}
